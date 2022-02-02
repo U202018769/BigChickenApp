87a386903ea89a37ade63db6c48eaec7e1730431
César Roberto López Barrantes <cesar.lopez.mail@gmail.com>
2022-02-02 17:27:05 -0500

    Pantalla de inicio creada

---
+++ app/src/main/java/com/grupo/bigchickenapp/BoldTextView.kt
@@ -0,0 +1,4 @@
+package com.grupo.bigchickenapp
+
+class BoldTextView {
+}
--- app/src/main/java/com/grupo/bigchickenapp/MainActivity.kt
+++ app/src/main/java/com/grupo/bigchickenapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.grupo.bigchickenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1500)
+        setTheme(R.style.Theme_BigChickenApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //para quitar la barra superior
+        //window.setFlags(
+        //    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //    WindowManager.LayoutParams.FLAG_FULLSCREEN
+        //)
+
     }
 }
