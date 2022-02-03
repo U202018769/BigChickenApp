9a1e50f1db66b811194ca8ffbe49ffa24ae61e77
César Roberto López Barrantes <cesar.lopez.mail@gmail.com>
2022-02-02 21:28:35 -0500

    Se diseño el Login

--- app/src/main/java/com/grupo/bigchickenapp/activities/LoginActivity.kt
+++ app/src/main/java/com/grupo/bigchickenapp/activities/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.grupo.bigchickenapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.grupo.bigchickenapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //para quitar la barra superior
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
